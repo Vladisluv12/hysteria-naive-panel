cbbd2ff8338be4d93d51dc34278fc5c3bd9b0e59
Vladisluv Chepurnykh <vlad.chepurnykh@gmail.com>
2026-06-19 18:44:36 +0300

    fix: add domain to Caddyfile site address in vps_test_install.sh

--- vps_test_install.sh
+++ vps_test_install.sh
@@ -239,7 +239,7 @@ if [[ $INSTALL_NAIVE -eq 1 ]]; then
   chown -R root:root /var/lib/naive
   {
     printf '{\n  auto_https off\n  order forward_proxy before file_server\n  servers :8443 {\n    protocols h1 h2\n  }\n}\n\n'
-    printf ':8443 {\n'
+    printf '%s:8443 {\n' "${DOMAIN}"
     if [[ "$TLS_MODE" == "letsencrypt" ]]; then
       printf '    tls %s\n\n' "${EMAIL}"
     else
